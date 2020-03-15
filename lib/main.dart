a7a36ebf6a666bfe22f7c854783d9e132206d58a
sondn-mog <sondn@mog.net>
2020-03-15 13:10:41 +0700

    loading

--- lib/main.dart
+++ lib/main.dart
@@ -16,40 +16,43 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-
   List data;
+  bool isLoading = false;
 
   Future<String> getData() async {
-        refreshKey.currentState?.show(atTop: false);
+    refreshKey.currentState?.show(atTop: false);
     //await Future.delayed(Duration(seconds: 2));
-
-
+    setState(() {
+      isLoading = true;
+    });
     var response = await http.get(
-      Uri.encodeFull("https://ikns.info/api/announcement_data.php"),
-      headers: {
-        "Accept": "application/json"
-      }
-    );
-
-
-      if (mounted) {
-        setState(() {
+        Uri.encodeFull("https://ikns.info/api/announcement_data.php"),
+        headers: {"Accept": "application/json"});
+    if (response.statusCode == 200) {
+      setState(() {
+        isLoading = false;
+      });
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+    if (mounted) {
+      setState(() {
         data = jsonDecode(response.body);
-        });
-      }
+      });
+    }
 
-    
     return "Success!";
   }
 
- var refreshKey = GlobalKey<RefreshIndicatorState>();
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
 
-@override
-    void initState() {
-      super.initState();
-        this.getData();
-    }
-
+  @override
+  void initState() {
+    super.initState();
+    this.getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,26 +60,25 @@ class HomePageState extends State<HomePage> {
       appBar: new AppBar(
         title: new Text("Listviews"),
       ),
-      body: RefreshIndicator(
+      body: isLoading ? Center(child: Text('Loading...'),) : RefreshIndicator(
         key: refreshKey,
-
- 
-              child: new ListView.builder(
+        child: new ListView.builder(
           itemCount: data == null ? 0 : data.length,
           itemBuilder: (BuildContext context, int index) {
-            
             return GestureDetector(
               onTap: () {
-                      Navigator.push(context, 
-                      new MaterialPageRoute(builder: (context) => DetailsPage(todo:Todo.fromJson(data[index])))
-                      );
-                                      },
-                        child: new Card(
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) =>
+                            DetailsPage(todo: Todo.fromJson(data[index]))));
+              },
+              child: new Card(
                 child: new Text(data[index]["title"]),
               ),
             );
           },
-        ), 
+        ),
         onRefresh: getData,
       ),
     );
@@ -84,30 +86,25 @@ class HomePageState extends State<HomePage> {
 }
 
 class Todo {
-
   final String title;
   final String description;
-  Todo(this.description,this.title);
+  Todo(this.description, this.title);
 
-  Todo.fromJson(
-    Map<String, dynamic> json) : 
-    title = json['title'], 
-    description = json['description'];
-
+  Todo.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        description = json['description'];
 }
-
 
 class DetailsPage extends StatelessWidget {
   final Todo todo;
-  
-   DetailsPage({Key key, @required this.todo}) : super(key: key);
+
+  DetailsPage({Key key, @required this.todo}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: new AppBar(
+      appBar: new AppBar(
         title: new Text(todo.title),
       ),
-
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Text(todo.description),
